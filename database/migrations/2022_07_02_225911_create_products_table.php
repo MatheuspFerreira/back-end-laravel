bc8f7de6158fe5ae32f52feb16b5841abc189945
Matheus Ferreira <alves.mfp@gmail.com>
2022-07-11 21:17:10 -0300

    Fix bug

--- database/migrations/2022_07_02_225911_create_products_table.php
+++ database/migrations/2022_07_02_225911_create_products_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->string('payment');
             $table->string('starcondition');
             $table->string('productimg');
+            
             $table->timestamps();
         });
     }
